1f94ca3da041cfede4f100e3da5f7989ab9ac4f6
DonLeo <leonard.donavan@gmail.com>
2026-05-25 22:37:22 +0200

    fix(program): checkbox pour collapse toggle + fix flèche mobile

---
+++ test_gemini.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\HttpClient\HttpClient;
+
+$client = HttpClient::create();
+$key = 'AIzaSyBpaaJUXWeSXi3zBMv33bq3hGvT6qtVgOE';
+$url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' . $key;
+
+$response = $client->request('POST', $url, [
+    'json' => [
+        'contents' => [
+            ['parts' => [['text' => 'Test']]]
+        ]
+    ]
+]);
+
+echo $response->getStatusCode() . "\n";
+print_r($response->toArray(false));
