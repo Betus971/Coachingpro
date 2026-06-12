7e531b24966ec792c4b10411c40a5693d8c0bf83
DonLeo <leonard.donavan@gmail.com>
2026-06-12 13:45:39 +0200

    fix: resolve connection persistence and cookie redirect loop issues

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -17,7 +17,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/chat', name: 'app_chat_')]
-#[IsGranted('IS_AUTHENTICATED_FULLY')]
+#[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
 class ChatController extends AbstractController
 {
     /**
